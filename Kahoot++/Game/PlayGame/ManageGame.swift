fc770dcd436cd7ce843aede31a6e85d0b5fe00dc
ipoogleduck <ipoogleduck@gmail.com>
2021-12-06 20:16:46 -0800

    As a test, added documentation to the ManageGame file

--- Kahoot++/Game/PlayGame/ManageGame.swift
+++ Kahoot++/Game/PlayGame/ManageGame.swift
@@ -9,6 +9,13 @@ import Foundation
 import UIKit
 import ALRT
 
+/// This enum contains the possible answer types for questions.
+///
+/// Call the displayName function to return a string of how the answer type is written in plain text
+///
+/// ```
+/// func displayName() -> String
+/// ```
 enum QuestionType: Codable, CaseIterable {
     case textAnswer
     case multipleChoice
@@ -26,31 +33,43 @@ enum QuestionType: Codable, CaseIterable {
     }
 }
 
+/// This structure for multiple choice questions conatains an answer to the multiple choice question and whether it is correct or not
 struct MultipleChoice: Codable {
     var answer: String
     var correct: Bool
 }
 
+/// This structure contains a single question.
+/// It mainly contains the question itself, the type of question, the answer(s), and the amount of points
 struct QuestionStruct: Codable {
-    var question: String
-    var type: QuestionType
-    var trueOrFalse: Bool = true
-    var multipleChoice: [MultipleChoice]?
-    var textAnswer: [String]?
+    var question: String //The question being asked as a string
+    var type: QuestionType //This is used by the code to deturmin the question types as some of the below variables are optional but may contain data even when not selected
+    var trueOrFalse: Bool = true //Default is true so that there is an option selected by default if the teacher sets the question to true/false
+    var multipleChoice: [MultipleChoice]? //An array of 4 multiple choice questions
+    var textAnswer: [String]? //An array of correct answers (there can be more than one if needed)
     var points: Int
 }
 
 extension SubjectTVC: GameDelegate {
-
+    
+    /// Checks if course questions are empty.
+    /// If empty, it will display an alert telling the user that no questions have been set by the instructor.
+    /// Otherwise, it will shuffle the questions and call *continueGame()*
+    /// - Parameter viewController: the view controller from which the next VC will be presented from
     func startGame(from viewController: UIViewController) {
         if course.questions.isEmpty {
-            ALRT.create(.alert, title: "Game Unavailable", message: "The instructor has not set any questions for this game").addOK().show()
+            ALRT.create(.alert, title: "Game Unavailable", message: "The instructor has not set any questions for this game").addOK().show() //Shows alert
         } else {
-            course.questions.shuffle()
+            course.questions.shuffle() //Randomizes questions
             continueGame(from: viewController)
         }
     }
-
+    
+    
+    /// Checks if it's ran through all the available questions.
+    /// If it has, it will display the *doneWithGameVC*, which contains an option for viewing the scoreboard, replaying the game, or exiting.
+    /// If there are still questiosn left, the function will present the corresonding view controller
+    /// - Parameter viewController: the view controller from which the next VC will be presented from
     func continueGame(from viewController: UIViewController) {
         if currentQuestionIndex < course.questions.count {
             let currentQuestion = course.questions[currentQuestionIndex]
@@ -91,8 +110,13 @@ extension SubjectTVC: GameDelegate {
         }
     }
     
+    
+    /// This function is called by one if the question answer VC's and displays info on the current question and if it was correct, as well as the leaderboard.
+    /// - Parameters:
+    ///   - viewController: the view controller from which the next VC will be presented from.
+    ///   - correct: whether the student selected the correct question.
     func showFeedback(from viewController: UIViewController, correct: Bool) {
-        let currentQuestion = course.questions[currentQuestionIndex]
+        let currentQuestion = course.questions[currentQuestionIndex] //Gets "last" question from course
         
         guard let FeedbackVC = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "FeedbackVC") as? FeedbackVC else {
             fatalError("Unable to Instantiate View Controller")
@@ -105,13 +129,16 @@ extension SubjectTVC: GameDelegate {
         FeedbackVC.correct = correct
         FeedbackVC.leaderboard = exampleLeaderboard
         
-        currentQuestionIndex += 1
+        currentQuestionIndex += 1 //Updates status to next question
         
         viewController.present(FeedbackVC, animated: true)
     }
     
 }
 
+
+/// This delegate is used by *TextAnswerVC*, *MCVC*, and *TFVC* which will call *showFeedback()* when the question has been answered in order to  dispaly *FeedbackVC*.
+/// It can also be used by *FeedbackVC* to call *continueGame()* which will display the next question or end the game
 protocol GameDelegate {
     func continueGame(from viewController: UIViewController)
     func showFeedback(from viewController: UIViewController, correct: Bool)
